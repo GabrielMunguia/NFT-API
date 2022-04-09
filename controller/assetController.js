1bbfed9f83cb2f5286d28fbf47ad776c6c672e43
Carlos Rafael Padilla <carlos09.cp65@gmail.com>
2022-04-08 21:38:47 -0600

    rarity

--- controller/assetController.js
+++ controller/assetController.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const Nft= require('../models/Nft')
+const Nft = require("../models/Nft");
 const { response, request } = require("express");
 const OpenseaScraper = require("opensea-scraper");
 const { formatPriceAsset } = require("../helpers/formateadorPrecio");
@@ -59,7 +59,6 @@ const getFullAsets = async (req = request, res = response) => {
     //traer todos los items de ese slug y los guarda en un array
     let assets = [];
 
-
     //let numeroDeItemsConTrait = 0;
 
     // Do while mientras el objeto next no sea null
@@ -91,55 +90,124 @@ const getFullAsets = async (req = request, res = response) => {
       await new Promise((resolve) => setTimeout(resolve, 2000));
     } while (next !== null);
 
+    // for (let i = 0; i < assets.length; i++) {
+    //   let suma = 0;
 
+    //   assets[i].rarityScore = assets[i].traits.reduce((acc, cur) => {
+    //     let rarity = cur.trait_count / assets.length;
+    //     let score = 1 / rarity;
+    //     suma = suma + score;
+    //     return suma;
+    //   }, 0);
+    // }
 
+    //Sacar el rarity score de cada item
 
+    let tally = { TraitCount: {} };
 
     for (let i = 0; i < assets.length; i++) {
-      let suma = 0;
+      let nftTraits = assets[i].map((e) => e.trait_type);
+      let nftValues = assets[i].map((e) => e.trait_value);
+      let numOfTraits = nftTraits.length;
 
-      assets[i].rarityScore = assets[i].traits.reduce((acc, cur) => {
-        let rarity = cur.trait_count / assets.length;
-        let score = 1 / rarity;
-        suma = suma + score;
-        return suma;
-      }, 0);
+      if (tally.TraitCount[numOfTraits]) {
+        tally.TraitCount[numOfTraits]++;
+      } else {
+        tally.TraitCount[numOfTraits] = 1;
+      }
+
+      for (let j = 0; j < nftTraits.length; j++) {
+        let current = nftTraits[j];
+        if (tally[current]) {
+          tally[current].occurences++;
+        } else {
+          tally[current] = { occurences: 1 };
+        }
+
+        let currentValue = nftValues[j];
+        if (tally[current][currentValue]) {
+          tally[current][currentValue]++;
+        } else {
+          tally[current][currentValue] = 1;
+        }
+
+      }
     }
+    ///--------------
+
+    const collectionAttributes = Object.keys(tally);
+    const nftArr=[];
+
+    for(let i = 0; i < assets.length; i++){
+      let current= assets[i];
+      let totalRarity = 0;
+
+      for(let j = 0; j < current.traits.length; j++){
+        let rarityScore= 1/(tally[current[j].trait_type][current[j].trait_value]/assets.length);
+        current.rarityScore= rarityScore;
+        totalRarity+= rarityScore;
+      }
+
+      let rarityScoreNumTraits= 1/(tally.TraitCount[Object.keys(current).length]/assets.length);
+
+      current.push({
+        trait_type:"TraitCount",
+        value:Object.keys(current).length,
+        rarityScore:rarityScoreNumTraits
+      });
+      totalRarity+= rarityScoreNumTraits;
+
+
+
+      if(current.length<collectionAttributes.length){
+        let nftAtributes= current.map((e)=>e.trait_type);
+        let absent= collectionAttributes.filter((e)=>!nftAtributes.includes(e));
+
+        absent.forEach(type=>{
+          let rarityScoreNull= 1/((assets.length-tally[type].occurences)/assets.length);
+          current.push({
+            trait_type:type,
+            value:null,
+            rarityScore:rarityScoreNull,
+          });
+          totalRarity+= rarityScoreNull;
+        });
+
+      }
+      assets[i].rarityScore= totalRarity;
+      assets[i].rarityScoreNumTraits= rarityScoreNumTraits;
+      
+
+    }
+    //-----------
 
 
 
     assets.sort((a, b) => b.rarityScore - a.rarityScore);
 
-
-
     for (let i = 0; i < assets.length; i++) {
       assets[i].rank = i + 1;
     }
 
-
-    assets.map(async (asset)=>{
-      const price =asset.last_sale !== null
-      ? formatPriceAsset(
-        asset.last_sale.payment_token.decimals,
-        asset.last_sale.total_price
-        )
-      : 0;
-      const data={
-        name:asset.name,
-        slug:asset.collection.slug,
-        price:asset.num_sales,
-        image:asset.image_original_url,
-        rank:asset.rank,
-        permalink:asset.permalink,
-        
-
-
-      }
+    assets.map(async (asset) => {
+      const price =
+        asset.last_sale !== null
+          ? formatPriceAsset(
+              asset.last_sale.payment_token.decimals,
+              asset.last_sale.total_price
+            )
+          : 0;
+      const data = {
+        name: asset.name,
+        slug: asset.collection.slug,
+        price: asset.num_sales,
+        image: asset.image_original_url,
+        rank: asset.rank,
+        permalink: asset.permalink,
+      };
       const nft = new Nft(data);
       await nft.save();
-    })
-
-
+    });
 
     //guardar archivo json
     // const data = JSON.stringify(assets);
@@ -148,12 +216,6 @@ const getFullAsets = async (req = request, res = response) => {
     //   if (err) throw err;
     //   console.log("The file has been saved!");
     // });
-
-
-
-
-
-    
 
     res.json({
       status: true,
@@ -170,18 +232,17 @@ const getFullAsets = async (req = request, res = response) => {
   }
 };
 
-
-const getFullAsetsBySlug = async( req  , res ) => {
-const { slug } = req.params;
-  const assets = await Nft.findAll( {
+const getFullAsetsBySlug = async (req, res) => {
+  const { slug } = req.params;
+  const assets = await Nft.findAll({
     where: {
-      slug
-    }
+      slug,
+    },
   });
-  res.json({size:assets.length, assets });
-}
+  res.json({ size: assets.length, assets });
+};
 module.exports = {
   getAssets,
   getFullAsets,
-  getFullAsetsBySlug
+  getFullAsetsBySlug,
 };
